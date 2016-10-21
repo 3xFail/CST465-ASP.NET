d1f8b106a5870c8abc7e05bd019f100f021af68e
Ryan <ryan.williams@oit.edu>
2016-10-20 18:56:30 -0700

    Finally got part 2 of lab 4 to work

--- ASP.Net Project/ASP.Net Project/Global.asax.cs
+++ ASP.Net Project/ASP.Net Project/Global.asax.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
+using System.Web.Http;
+using CST465Project;
 
 namespace ASP.Net_Project
 {
-    public class Global : System.Web.HttpApplication
+    public class Global : HttpApplication
     {
         protected void Application_Start(object sender, EventArgs e)
         {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
