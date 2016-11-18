aa2e8a5bed97a1eede0b1f52096e60450ef344ad
Ryan <ryan.williams@oit.edu>
2016-11-17 19:21:44 -0800

    Files are somewhere between started and almost done.. for Lab 8 setup

---
+++ ASP.Net Project/ASP.Net Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.Net_Project.Startup))]
+namespace ASP.Net_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
